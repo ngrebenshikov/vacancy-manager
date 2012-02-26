1dea866fa86b2d6f5081480ff6da71e058e20c45
user <user@user-??>
2012-02-26 19:56:33 +0800

    ???????

---
+++ sandbox/SergeyS/summa/Project3/CodeFile1.cs
@@ -0,0 +1,29 @@
+﻿/*Всё, что от вас требуется — найти сумму всех целых чисел, лежащих между 1 и N включительно.*/
+using System;
+class main
+{
+    public static int Main()
+    {
+        int n, i, s;
+        s = 0;
+        ///Console.Write("Введите число N: ");
+        n = Convert.ToInt16(Console.ReadLine());
+        if (n <= 0)
+        {
+            for (i = n; i <= 1; i++)
+            {
+                s = s + i;
+            }
+        }
+        else
+        {
+            for (i = 1; i <= n; i++)
+            {
+                s = s + i;
+            }
+        }
+        Console.WriteLine(s);
+        //Console.ReadLine();
+        return 0;
+    }
+}
