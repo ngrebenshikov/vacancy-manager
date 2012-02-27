60a91cd38b618a989ab335b6c267833d6cdf9a90
Valentin Banshchikov <isalem100@gmail.com>
2012-02-27 21:21:10 +0800

    ??????? ????? ???????????? ????????? ? ??????? ????????

---
+++ sandbox/Anatoly/Vacan/Models/Applicant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Applican.Models
+{
+  public class Applicant
+  {
+
+    public int ID { get; set; }
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public string MiddleName { get; set; }
+    public string EMail { get; set; }
+    public string FileName { get; set; }
+  }
+
+  public class ApplicantDbContext : DbContext
+  {
+    public DbSet<Applicant> Applicants { get; set; }
+  }
+}
---
+++ sandbox/Bnschikov/VMProject/VMProject/Models/Applicant.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace VMProject.Models
+{
+    public class Applicant
+    {
+        public int ApplicantID { get; set; }
+        public string FullName { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Vacancy> Vacancies { get; set; }
+    }
+}
