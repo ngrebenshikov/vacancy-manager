29bb3b69bc8942fcb20e0beccf1b59be150a584b
user <user@user-??>
2012-02-28 14:50:25 +0800

    ???????

---
+++ src/VacancyManager/VacancyManager/Models/Consideration.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace VacancyManager.Models
+{
+    /// <summary>
+    /// Класс, описывающий сущность рассмотрения заявки. В диаграмме представлен как таблица СоискательВакансия.
+    /// </summary>
+    public class Consideration
+    {
+        public int ConsiderationID { get; set; }
+        public int VacancyID { get; set; }
+        public int ApplicantID { get; set; }
+
+        public virtual Applicant Applicant { get; set; }
+        public virtual Vacancy Vacancy { get; set; }
+        public virtual ICollection<Commentary> Commentaries { get; set; } 
+    }
+}
