02501001361d918d7a0b2926d3c58a3c6f6954bb
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2012-03-20 17:23:25 +0800

    ?????? ? ExtJS

---
+++ src/VacancyManager/VacancyManager/Services/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Security;
+using VacancyManager.Models;
+
+namespace VacancyManager.Services
+{
+    public interface IRepository : IDisposable
+    {
+        IEnumerable<Vacancy> AllVisibleVacancies();
+        User GetUserByEmail(string email);
+        User GetUserByUsername(string username);
+        MembershipUser GetMembershipUserByUserName(string username);
+        void CreateUser(string username, string password, string email);
+        bool ValidateUser(string username, string password);
+    }
+}
