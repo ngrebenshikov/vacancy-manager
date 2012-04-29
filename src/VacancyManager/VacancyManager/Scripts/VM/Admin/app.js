e0613fa1ac178ea607a8997776c09c578200e88d
???????? ???????? <tupieurods@gmail.com>
2012-04-29 19:07:02 +0800

    ??????? ???? ??????? Authorize ????????? ????????????? Access Denied ?????? ???? ???????????? ?? ?????????, ? ??????? ??? ?? ??????(???? ??? ?????) ?? ?????? ?????? BaseStore ?? ?????, ???? ?? ???????????? ????? ???? ??????         ????? exception Store, ?? ????? ????? ?????? ??? ??????.

--- src/VacancyManager/VacancyManager/Scripts/VM/Admin/app.js
+++ src/VacancyManager/VacancyManager/Scripts/VM/Admin/app.js
@@ -35,3 +35,94 @@ Ext.application
     }
   }
 );
+
+var PreviousRequest;
+
+Ext.Ajax.on('requestcomplete', function (connection, response)
+{
+  try
+  {
+    if (response.responseText)
+    {
+      var result = Ext.JSON.decode(response.responseText);
+      if (result.success === false)
+      {
+        switch (result.message)
+        {
+          case '401':
+            PreviousRequest = response.request.options;
+            login_form = new Ext.form.FormPanel(
+            {
+              labelWidth: 55,
+              frame: true,
+              defaultType: 'textfield',
+              items:
+              [
+                { fieldLabel: 'Login', name: 'login', anchor: '100%' },
+                { fieldLabel: 'Password', name: 'password', inputType: 'password', anchor: '100%' }
+              ]
+            });
+            login_window = new Ext.Window(
+            {
+              title: 'Login form',
+              width: 300,
+              height: 150,
+              layout: 'fit',
+              plain: true,
+              closable: false,
+              bodyStyle: 'padding:5px;',
+              items: login_form,
+              buttons:
+              [
+                { text: 'Login',
+                  handler: function ()
+                  {
+                    login_window.getEl().mask('Login...');
+                    Ext.Ajax.request(
+                    {
+                      url: '../../Account/ExtJSLogOn',
+                      params:
+                      { login: login_form.getForm().getValues().login,
+                        password: login_form.getForm().getValues().password
+                      },
+                      success: function (result, request)
+                      {
+                        var JsonResult = Ext.JSON.decode(result.responseText);
+                        if (JsonResult.LogOnResult != '')
+                        {
+                          Ext.MessageBox.show(
+                          {
+                            title: 'Error',
+                            msg: JsonResult.LogOnResult,
+                            minWidth: 200,
+                            bottons: Ext.MessageBox.OK,
+                            icon: Ext.MessageBox.WARNING
+                          });
+                          login_window.getEl().unmask(true);
+                        }
+                        else
+                        {
+                          //Повторение предыдущего запроса(пока только одного)
+                          //Нужно протестировать, возможно из PreviousRequest нужно сделать массив
+                          Ext.Ajax.request(PreviousRequest);
+                          login_window.close();
+                        }
+                      }
+                    });
+                  }
+                },
+                { text: 'Cancel',
+                  handler: function () { login_window.close(); }
+                }
+              ]
+            });
+            login_window.show();
+            break;
+            Ext.Msg.alert('Error', result.message);
+        }
+      }
+    }
+  } catch (err)
+  {
+  }
+});
