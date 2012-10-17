c23dc385c0e79251aec1a6323a1695cd40551624
Ivan <Ivan@IvanPC>
2012-10-17 22:37:27 +0800

    ???????? ???????????? ? ??????????

---
+++ src/VacancyManager/VacancyManager/Models/Applicant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace VacancyManager.Models
+{
+    public class Applicant
+    {   
+        [Key]
+        public int ApplicantID { get; set; }
+        public string FIO { get; set; }
+        public string ContactPhone { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<Resume> Resumes { get; set; }
+        public virtual ICollection<Consideration> Considerations { get; set; }
+        public virtual ICollection<File> Files { get; set; }
+    }
+}
