29f39a2493f60545dafeccb911d80cd8e11815e0
???????? ???????? <tupieurods@gmail.com>
2012-12-24 20:51:41 +0800

    Issue 57: ????????????? ????????? ????????? ?? ???????? ? consideration ???????.

--- src/VacancyManager/VacancyManager/Services/Managers/CommentsManager.cs
+++ src/VacancyManager/VacancyManager/Services/Managers/CommentsManager.cs
@@ -6,61 +6,62 @@ using VacancyManager.Models;
 
 namespace VacancyManager.Services.Managers
 {
-    public class CommentsManager
+  public class CommentsManager
+  {
+
+
+    internal static IEnumerable<Comment> GetComments(int considerationId)
     {
+      VacancyContext _db = new VacancyContext();
+      return _db.Commentaries.Where(v => v.Consideration.ConsiderationID == considerationId).ToList();
+    }
 
+    internal static Comment GetComment(int Id)
+    {
+      VacancyContext _db = new VacancyContext();
+      return _db.Commentaries.Where(v => v.CommentID == Id).SingleOrDefault();
+    }
 
-        internal static IEnumerable<Comment> GetComments(int considerationId)
-        {
-            VacancyContext _db = new VacancyContext();
-            return _db.Commentaries.Where(v => v.Consideration.ConsiderationID == considerationId).ToList();
-        }
+    internal static IEnumerable<Comment> CreateComment(int considerationId, int? userId, string body, string commenterName)
+    {
+      VacancyContext _db = new VacancyContext();
 
-        internal static Comment GetComment(int Id)
-        {
-            VacancyContext _db = new VacancyContext();
-            return _db.Commentaries.Where(v => v.CommentID == Id).SingleOrDefault();
-        }
-
-        internal static IEnumerable<Comment> CreateComment(int considerationId, int userId, string body)
-        {
-            VacancyContext _db = new VacancyContext();
-
-            var NewComment = new List<Comment> {
+      var NewComment = new List<Comment> {
                 new Comment
             {
                 ConsiderationID = considerationId,
                 Body = body,
                 CreationDate = DateTime.Now,
-                UserID = userId
+                UserID = userId,
+                CommenterName = commenterName
             }};
 
-            _db.Commentaries.Add(NewComment.First());
-            _db.SaveChanges();
+      _db.Commentaries.Add(NewComment.First());
+      _db.SaveChanges();
 
-            return NewComment.ToList();
-        }
-
-        internal static void UpdateComment(int commentId, string body)
-        {
-            VacancyContext _db = new VacancyContext();
-            var UpdateComment = _db.Commentaries.SingleOrDefault(a => a.CommentID == commentId);
-            if (UpdateComment != null)
-            {
-                UpdateComment.Body = body;
-                _db.SaveChanges();
-            }
-        }
-        internal static void DeleteComment(int commentId)
-        {
-            VacancyContext _db = new VacancyContext();
-            var DeleteComment = _db.Commentaries.SingleOrDefault(a => a.CommentID == commentId);
-
-            if (DeleteComment == null) return;
-
-            _db.Commentaries.Remove(DeleteComment);
-            _db.SaveChanges();
-        }
-
+      return NewComment.ToList();
     }
+
+    internal static void UpdateComment(int commentId, string body)
+    {
+      VacancyContext _db = new VacancyContext();
+      var UpdateComment = _db.Commentaries.SingleOrDefault(a => a.CommentID == commentId);
+      if (UpdateComment != null)
+      {
+        UpdateComment.Body = body;
+        _db.SaveChanges();
+      }
+    }
+    internal static void DeleteComment(int commentId)
+    {
+      VacancyContext _db = new VacancyContext();
+      var DeleteComment = _db.Commentaries.SingleOrDefault(a => a.CommentID == commentId);
+
+      if (DeleteComment == null) return;
+
+      _db.Commentaries.Remove(DeleteComment);
+      _db.SaveChanges();
+    }
+
+  }
 }
