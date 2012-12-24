2ffe06a0d97bfd3d46a647520bd2d33dfc39962a
???????? ???????? <tupieurods@gmail.com>
2012-12-24 20:51:41 +0800

    Issue 57: ????????????? ????????? ????????? ?? ???????? ? consideration ???????.

--- src/VacancyManager/VacancyManager/Controllers/CommentsController.cs
+++ src/VacancyManager/VacancyManager/Controllers/CommentsController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using VacancyManager.Services;
 using VacancyManager.Services.Managers;
@@ -12,143 +10,143 @@ using System.Web.Security;
 
 namespace VacancyManager.Controllers
 {
-    public class CommentsController : BaseController
+  public class CommentsController : BaseController
+  {
+    //
+    JavaScriptSerializer jss = new JavaScriptSerializer();
+    // GET: /Comments/
+    [HttpGet]
+    public JsonResult Load(int considerationId)
     {
-        //
-        JavaScriptSerializer jss = new JavaScriptSerializer();
-        // GET: /Comments/
-        [HttpGet]
-        public JsonResult Load(int considerationId)
-        {
-            bool LoadSuccess = true;
-            string LoadMessage = "Комментариии успешно загружены";
-            var Comments = CommentsManager.GetComments(considerationId);
+      bool LoadSuccess = true;
+      string LoadMessage = "Комментариии успешно загружены";
+      var Comments = CommentsManager.GetComments(considerationId);
 
-            var CommentsList = (from comms in Comments
-                                orderby comms.CommentID descending
-                                select new
-                                 {
-                                     CommentID = comms.CommentID,
-                                     CreationDate = comms.CreationDate.ToShortDateString(),
-                                     Body = comms.Body,
-                                     UserID = comms.User.UserID,
-                                     User = comms.User.UserName,
-                                     ConsiderationID = comms.Consideration.ConsiderationID
-                                 }
+      var CommentsList = (from comms in Comments
+                          orderby comms.CommentID descending
+                          select new
+                           {
+                             CommentID = comms.CommentID,
+                             CreationDate = comms.CreationDate.ToShortDateString(),
+                             Body = comms.Body,
+                             UserID = comms.User != null ? comms.User.UserID : -1,
+                             CommentatorName = comms.CommenterName,
+                             ConsiderationID = comms.Consideration.ConsiderationID
+                           }
 
-                             ).ToList();
+                       ).ToList();
 
 
 
-            return Json(new
-          {
-              comments = CommentsList,
-              success = LoadSuccess,
-              message = LoadMessage
-          }, JsonRequestBehavior.AllowGet);
-        }
-
-
-        //
-        // GET: /Comments/Create
-        [HttpPost]
-        public ActionResult Create(string comments)
-        {
-            bool CreateSuccess = false;
-            string CreateMessage = "При создании комментария произошла ошибка";
-            object NewComment = null;
-            if (comments != null)
-            {
-                var Comment = jss.Deserialize<dynamic>(comments);
-                string Body = Comment["Body"].ToString();
-                int ConsiderationID = Convert.ToInt32(Comment["ConsiderationID"]);
-                string CurrentUserName = System.Web.HttpContext.Current.User.Identity.Name;
-                VMMembershipUser CurrentUser = (VMMembershipUser)Membership.GetUser(CurrentUserName);
-                Int32 CurrentUserKey = Convert.ToInt32(CurrentUser.ProviderUserKey);
-                //CreatedComment = (CommentsManager.CreateComment(ConsiderationID, CurrentUserKey, Body)).ToList();
-                Comment CreatedComment = (CommentsManager.CreateComment(ConsiderationID, CurrentUserKey, Body)).SingleOrDefault();
-                CreateSuccess = true;
-
-                bool isSent = MailSender.SendMessageToApplicant(CreatedComment.CommentID);
-                //bool isSent = MailSender.SendMessageToAdmins(CreatedComment.CommentID);
-
-                CreateMessage = isSent ? "Комментарий успешно добавлен. Письмо отправлено" : "Комментарий успешно добавлен. Письмо не отправлено";
-
-                //var NewComment = (from comms in CreatedComment
-                //                  select new
-                //                  {
-                //                      CommentID = comms.CommentID,
-                //                      CreationDate = comms.CreationDate.ToShortDateString(),
-                //                      Body = comms.Body,
-                //                      UserID = comms.UserID,
-                //                      User = "",
-                //                      ConsiderationID = comms.ConsiderationID
-                //                  }
-                //            ).ToList();
-
-                NewComment = new
-                {
-                    CommentID = CreatedComment.CommentID,
-                    CreationDate = CreatedComment.CreationDate.ToShortDateString(),
-                    Body = CreatedComment.Body,
-                    UserID = CreatedComment.UserID,
-                    User = CurrentUserName,
-                    ConsiderationID = CreatedComment.ConsiderationID
-                };
-            }
-            
-            return Json(new
-            {
-                data = NewComment,
-                success = CreateSuccess,
-                message = CreateMessage
-            }, JsonRequestBehavior.DenyGet);
-        }
-
-        //
-        // GET: /Comments/Edit/5
-        [HttpPost]
-        public ActionResult Update(string comments)
-        {
-            bool UpdateSuccess = false;
-            string UpdateMessage = "При обновлении комментария произошла ошибка";
-
-            if (comments != null)
-            {
-                var UpdateComment = jss.Deserialize<dynamic>(comments);
-                CommentsManager.UpdateComment(Convert.ToInt32(UpdateComment["CommentID"]),
-                                              UpdateComment["Body"]);
-                UpdateSuccess = true;
-                UpdateMessage = "Комментарий успешно обновлен";
-            }
-
-            return Json(new
-            {
-                success = UpdateSuccess,
-                message = UpdateMessage
-            }, JsonRequestBehavior.DenyGet);
-        }
-
-        // GET: /Comments/Delete/5
-
-        public ActionResult Delete(string comments)
-        {
-            bool DeleteSuccess = false;
-            string DeleteMessage = "При удалении комментария произошла ошибка";
-
-            if (comments != null)
-            {
-                var DeleteComment = jss.Deserialize<dynamic>(comments);
-                CommentsManager.DeleteComment(Convert.ToInt32(DeleteComment["CommentID"]));
-                DeleteSuccess = true;
-                DeleteMessage = "Комментарий успешно удален";
-            }
-
-            return Json(new
-            {
-                success = DeleteSuccess,
-                message = DeleteMessage
-            }, JsonRequestBehavior.DenyGet);
-        }
+      return Json(new
+    {
+      comments = CommentsList,
+      success = LoadSuccess,
+      message = LoadMessage
+    }, JsonRequestBehavior.AllowGet);
     }
+
+
+    //
+    // GET: /Comments/Create
+    [HttpPost]
+    public ActionResult Create(string comments)
+    {
+      bool CreateSuccess = false;
+      string CreateMessage = "При создании комментария произошла ошибка";
+      object NewComment = null;
+      if (comments != null)
+      {
+        var Comment = jss.Deserialize<dynamic>(comments);
+        string Body = Comment["Body"].ToString();
+        int ConsiderationID = Convert.ToInt32(Comment["ConsiderationID"]);
+        string CurrentUserName = System.Web.HttpContext.Current.User.Identity.Name;
+        VMMembershipUser CurrentUser = (VMMembershipUser)Membership.GetUser(CurrentUserName);
+        Int32 CurrentUserKey = Convert.ToInt32(CurrentUser.ProviderUserKey);
+        //CreatedComment = (CommentsManager.CreateComment(ConsiderationID, CurrentUserKey, Body)).ToList();
+        Comment CreatedComment = (CommentsManager.CreateComment(ConsiderationID, CurrentUserKey, Body, CurrentUser.UserName)).SingleOrDefault();
+        CreateSuccess = true;
+
+        bool isSent = MailSender.SendMessageToApplicant(CreatedComment.CommentID);
+        //bool isSent = MailSender.SendMessageToAdmins(CreatedComment.CommentID);
+
+        CreateMessage = isSent ? "Комментарий успешно добавлен. Письмо отправлено" : "Комментарий успешно добавлен. Письмо не отправлено";
+
+        //var NewComment = (from comms in CreatedComment
+        //                  select new
+        //                  {
+        //                      CommentID = comms.CommentID,
+        //                      CreationDate = comms.CreationDate.ToShortDateString(),
+        //                      Body = comms.Body,
+        //                      UserID = comms.UserID,
+        //                      User = "",
+        //                      ConsiderationID = comms.ConsiderationID
+        //                  }
+        //            ).ToList();
+
+        NewComment = new
+        {
+          CommentID = CreatedComment.CommentID,
+          CreationDate = CreatedComment.CreationDate.ToShortDateString(),
+          Body = CreatedComment.Body,
+          UserID = CreatedComment.UserID,
+          User = CurrentUserName,
+          ConsiderationID = CreatedComment.ConsiderationID
+        };
+      }
+
+      return Json(new
+      {
+        data = NewComment,
+        success = CreateSuccess,
+        message = CreateMessage
+      }, JsonRequestBehavior.DenyGet);
+    }
+
+    //
+    // GET: /Comments/Edit/5
+    [HttpPost]
+    public ActionResult Update(string comments)
+    {
+      bool UpdateSuccess = false;
+      string UpdateMessage = "При обновлении комментария произошла ошибка";
+
+      if (comments != null)
+      {
+        var UpdateComment = jss.Deserialize<dynamic>(comments);
+        CommentsManager.UpdateComment(Convert.ToInt32(UpdateComment["CommentID"]),
+                                      UpdateComment["Body"]);
+        UpdateSuccess = true;
+        UpdateMessage = "Комментарий успешно обновлен";
+      }
+
+      return Json(new
+      {
+        success = UpdateSuccess,
+        message = UpdateMessage
+      }, JsonRequestBehavior.DenyGet);
+    }
+
+    // GET: /Comments/Delete/5
+
+    public ActionResult Delete(string comments)
+    {
+      bool DeleteSuccess = false;
+      string DeleteMessage = "При удалении комментария произошла ошибка";
+
+      if (comments != null)
+      {
+        var DeleteComment = jss.Deserialize<dynamic>(comments);
+        CommentsManager.DeleteComment(Convert.ToInt32(DeleteComment["CommentID"]));
+        DeleteSuccess = true;
+        DeleteMessage = "Комментарий успешно удален";
+      }
+
+      return Json(new
+      {
+        success = DeleteSuccess,
+        message = DeleteMessage
+      }, JsonRequestBehavior.DenyGet);
+    }
+  }
 }
--- src/VacancyManager/VacancyManager/Models/Comment.cs
+++ src/VacancyManager/VacancyManager/Models/Comment.cs
@@ -2,16 +2,17 @@
 
 namespace VacancyManager.Models
 {
-    public class Comment
-    {
-        public int CommentID { get; set; }
-        public int UserID { get; set; }
-        public int ConsiderationID { get; set; }
-        public DateTime CreationDate { get; set; }
+  public class Comment
+  {
+    public int CommentID { get; set; }
+    public int? UserID { get; set; }
+    public int ConsiderationID { get; set; }
+    public DateTime CreationDate { get; set; }
 
-        public string Body { get; set; }
+    public string Body { get; set; }
+    public string CommenterName { get; set; }
 
-        public virtual User User { get; set; }
-        public virtual Consideration Consideration { get; set; }
-    }
+    public virtual User User { get; set; }
+    public virtual Consideration Consideration { get; set; }
+  }
 }
