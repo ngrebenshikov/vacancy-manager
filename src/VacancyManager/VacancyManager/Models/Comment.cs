a0c57cf40ee4604a10090477e057ab7b3fb0062d
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2012-12-25 18:10:57 +0800

    Backed out changeset: 496f99020f75

--- src/VacancyManager/VacancyManager/Models/Comment.cs
+++ src/VacancyManager/VacancyManager/Models/Comment.cs
@@ -5,7 +5,7 @@ namespace VacancyManager.Models
     public class Comment
     {
         public int CommentID { get; set; }
-        public Nullable<int> UserID { get; set; }
+        public int UserID { get; set; }
         public int ConsiderationID { get; set; }
         public DateTime CreationDate { get; set; }
 
