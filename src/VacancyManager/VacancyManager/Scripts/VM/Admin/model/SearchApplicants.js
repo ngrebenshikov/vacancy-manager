f0140d82778f8ee144f6fd42a7168eb9ca7ec5bd
budulay24rus <budulay24rus@localhost>
2013-06-08 09:19:26 +1100

    ???????? ??? ? ?????????? ??? ?????? ?????????? ??????????? ? ??????????

--- src/VacancyManager/VacancyManager/Scripts/VM/Admin/model/SearchApplicants.js
+++ src/VacancyManager/VacancyManager/Scripts/VM/Admin/model/SearchApplicants.js
@@ -2,7 +2,7 @@
     {
         extend: 'Ext.data.Model',
         idProperty: 'ApplicantID',
-        fields: ['ApplicantID', 'FullName', 'Requirements', 'Vacancies', 'Selected'],
+        fields: ['ApplicantID', 'FullName', 'Requirements', 'Vacancies'],
         proxy:
         {
             type: 'ajax',
@@ -16,19 +16,6 @@
                 type: 'json',
                 root: 'freeapplicants',
                 successProperty: 'success'
-            },
-
-            writer:
-            {
-                type: 'json',
-                encode: false,
-                listful: true,
-                writeAllFields: true,
-                getRecordData: function (record) {
-                    return { 'freeapplicants': Ext.JSON.encode(record.data)
-                    };
-                }
-            },
-            headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+            }
         }
     });
