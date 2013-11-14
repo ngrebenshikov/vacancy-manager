76280984cc9ea0216c733a043893a93c9cae9944
Nickolay Grebenshikov <Nickolay.Grebenshikov@lanit-tercom.com>
2013-11-14 11:35:59 +0800

    Fix

--- src/VacancyManager/VacancyManager/Scripts/VM/Admin/model/ApplicantResumeGrid.js
+++ src/VacancyManager/VacancyManager/Scripts/VM/Admin/model/ApplicantResumeGrid.js
@@ -2,5 +2,32 @@
     {
         extend: 'Ext.data.Model',
         idProperty: 'ResumeId',
-        fields: ['ResumeId', 'Date']
-    });
+        fields: ['ResumeId', 'Date'],
+        proxy: {
+            type: 'ajax',
+            api: {
+                read: '/Resume/LoadResume',
+                destroy: '/Resume/DeleteResume'
+            },
+
+            reader: {
+                type: 'json',
+                root: 'data',
+                successProperty: 'success'
+            },
+
+            writer:
+                {
+                    type: 'json',
+                    encode: false,
+                    listful: true,
+                    writeAllFields: true,
+                    getRecordData: function (record) {
+                        return {
+                            'data': Ext.JSON.encode(record.data)
+                        };
+                    }
+                },
+                headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+
+        }    });
--- src/VacancyManager/VacancyManager/Scripts/VM/Admin/model/ResumeApplicant.js
+++
@@ -1,7 +0,0 @@
-Ext.define('VM.model.ResumeApplicant',
-    {
-        extend: 'Ext.data.Model',
-        idProperty: 'ResumeId',
-        fields: ['ResumeId', 'ApplicantId', 'Date']
-    }
-);
